63c86c47120a1cf4fc70ef21e2d128bf3b3d2e78
smouchlianitis <smouchlianitis@vivawallet.com>
2025-06-17 14:51:33 +0300

    feat: otel setup

--- DescopeSampleApp/Global.asax.cs
+++ DescopeSampleApp/Global.asax.cs
@@ -8,18 +8,42 @@ using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace DescopeSampleApp
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private TracerProvider _tracerProvider;
+
         protected void Application_Start()
         {
+            _tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddAspNetInstrumentation()
+                .AddConsoleExporter()
+                .AddOtlpExporter(options =>
+                {
+                    options.Endpoint = new Uri("http://localhost:4318/v1/traces");
+                    options.Protocol = OtlpExportProtocol.HttpProtobuf;
+                })
+                .SetResourceBuilder(
+                    ResourceBuilder.CreateDefault()
+                        .AddService(serviceName: "DescopeSampleApp", serviceVersion: "1.0.0"))
+                .Build();
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_End()
+        {
+            _tracerProvider?.Dispose();
         }
     }
 }
